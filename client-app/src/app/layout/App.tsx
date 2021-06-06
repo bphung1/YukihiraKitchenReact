a061210bb4cc414eae244f0dc7304515a33c652f
Bao phung <b.giaphung@gmail.com>
2021-06-06 18:26:28 -0500

    error handling

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,31 +3,37 @@ import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import RecipeDashboard from "../../features/recipes/dashboard/RecipeDashboard";
 import { observer } from "mobx-react-lite";
-import { Route, useLocation } from "react-router";
+import { Route, Switch, useLocation } from "react-router";
 import HomePage from "../../features/home/HomePage";
 import RecipeForm from "../../features/recipes/forms/RecipeForm";
 import RecipeDetails from "../../features/recipes/details/RecipeDetails";
+import { ToastContainer } from "react-toastify";
+import NotFound from "../../features/errors/NotFound";
 
 function App() {
   const location = useLocation();
 
   return (
     <>
+      <ToastContainer position="bottom-right" hideProgressBar />
       <Route exact path="/" component={HomePage} />
-      {/* /(.+) means that any route that match the forward '/' plus(+) something else*/}
+      {/* /(.+) means that any route that match the '/' plus(+) something else*/}
       <Route
         path={"/(.+)"}
         render={() => (
           <>
             <NavBar />
             <Container style={{ marginTop: "7em" }}>
-              <Route exact path="/recipes" component={RecipeDashboard} />
-              <Route path="/recipes/:id" component={RecipeDetails} />
-              <Route
-                key={location.key}
-                path={["/createRecipe", "/manage/:id"]}
-                component={RecipeForm}
-              />
+              <Switch>
+                <Route exact path="/recipes" component={RecipeDashboard} />
+                <Route path="/recipes/:id" component={RecipeDetails} />
+                <Route
+                  key={location.key}
+                  path={["/createRecipe", "/manage/:id"]}
+                  component={RecipeForm}
+                />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </>
         )}
--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,12 +1,15 @@
 import { createContext, useContext } from "react";
+import CommonStore from "./commonStore";
 import RecipeStore from "./recipeStore";
 
 interface Store {
   recipeStore: RecipeStore;
+  commonStore: CommonStore;
 }
 
 export const store: Store = {
   recipeStore: new RecipeStore(),
+  commonStore: new CommonStore(),
 };
 
 export const StoreContext = createContext(store);
